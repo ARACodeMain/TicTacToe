d5f8e9310eb524096c2b6a132499042263306046
ARACodeMain <112445665+ARACodeMain@users.noreply.github.com>
2022-10-24 01:21:06 +0100

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,13 +1,14 @@
-# Simple Tic Tac Toe game    برنامج بسيط للعبة
+#1 Simple Tic Tac Toe
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 turn = 'X'
-
 def show_board():
-    print(f'|{board[0]}|{board[1]}|{board[2]}|\t 1 2 3')
-    print(f'|{board[3]}|{board[4]}|{board[5]}|\t 4 5 6')
-    print(f'|{board[6]}|{board[7]}|{board[8]}|\t 7 8 9')
+    print(f"|{board[0]}|{board[1]}|{board[2]}|  0 1 2")
+    print(f"|{board[3]}|{board[4]}|{board[5]}|  3 4 5")
+    print(f"|{board[6]}|{board[7]}|{board[8]}|  6 7 8")
+    
+show_board()
 
-def winner(board):
+def winner():
     if board[0] == board[1] == board[2] != ' ': return True
     if board[3] == board[4] == board[5] != ' ': return True
     if board[6] == board[7] == board[8] != ' ': return True
@@ -18,22 +19,21 @@ def winner(board):
     if board[2] == board[4] == board[6] != ' ': return True
     return False
 
-show_board()
 while True:
-    # Player input
-    move = int(input(f'Your turn {turn}')) - 1  # 0:8
-    if move >= 0 and move <= 8 and board[move] == ' ':
-        board[move] = turn
-        show_board()
-        # Check winner
-        if winner(board):
-            print(f'The Winner Is {turn}')
-            break
-        # Check Draw
-        if not ' ' in board:
-            print('It is a Draw!')
-            break        
-        # Switch turns
-        if turn == 'X':
-            turn = 'O'
-        else: turn = 'X'
+    move = input(f"Your move {turn} (0:8): ")
+    if move in ['0','1','2','3','4','5','6','7','8']:
+        move = int(move)
+        if board[move] == ' ':
+            board[move] = turn
+            show_board()
+            if winner():
+                print(f"The winner is {turn}")
+                break
+            if ' ' not in board:
+                print("It is a Draw XO!")
+                break       
+            # Switch turn
+            if turn == 'X':
+                turn = 'O'
+            else: turn = 'X'
+
